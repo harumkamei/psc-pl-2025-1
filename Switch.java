ae715341a67ed25c2f4dc8bde9215616ed45ee2c
harumkamei <harumikamei2019@gmail.com>
2025-03-21 11:49:01 -0300

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -1,49 +1,52 @@
+import javax.swing.JOptionPane;
+
 public class Switch {
     
     public static void main(String[] args) {
+                 JOptionPane.showInputDialog( null, " Digite um número " );
+
         int dia = 3;
         switch (dia) {
             case 1:
-                System.out.println(" Domingo ");
+                         JOptionPane.showMessageDialog( null, " Domingo " );
 
         }
                  switch (dia) {
             case 2:
-                System.out.println(" Segunda ");
+                     JOptionPane.showMessageDialog( null, " Segunda " );
                 break;
 
                  }
          switch (dia) {
             case 3:
-                System.out.println(" Terça ");
+                     JOptionPane.showMessageDialog( null, " Terça " );
                 break;
         
          }
          switch (dia) {
             case 4:
-                System.out.println(" Quarta ");
+                     JOptionPane.showMessageDialog( null, " Quarta " );
                 break;
          }
 
                  switch (dia) {
             case 5:
-                System.out.println(" Quinta ");
+         JOptionPane.showMessageDialog( null, " Quinta " );
                 break;
         }
 
                  switch (dia) {
             case 6:
-                System.out.println(" Sexta ");
+                         JOptionPane.showMessageDialog( null, " Sexta " );
                 break;
         }
          
                 switch (dia) {
             case 7:
-                System.out.println(" Sábado ");
+                        JOptionPane.showMessageDialog( null, " Sábado " );
                 break;
         
             default:
-                  System.out.println(" Digite um número entre 1 e 7 ");
                 break;
         }
     }
