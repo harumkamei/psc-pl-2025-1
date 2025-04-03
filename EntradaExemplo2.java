ed038c9a2d28fc5120804a1a0d73aa56d8c3e911
harumkamei <harumikamei2019@gmail.com>
2025-04-03 13:51:13 -0300

    Add files via upload

---
+++ EntradaExemplo2.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class EntradaExemplo2 {
+    
+    public static void main(String[] args) {
+        int resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Confirmação", JOptionPane.YES_NO_CANCEL_OPTION);
+
+        if(resposta == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, " Você escolheu continuar :) ");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, " Você escolheu sair :(");
+        }
+    }
+}
